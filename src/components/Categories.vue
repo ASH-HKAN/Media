c5e9bf698ad0007328d52ea4a51c17db2fbe6d39
ASH-HKAN <ashkan.b.m.zero@gmail.com>
2024-02-11 02:13:25 +0100

    cat Boxes

--- src/components/Categories.vue
+++ src/components/Categories.vue
@@ -2,47 +2,49 @@
   <v-app class="main-app">
     <div class="ttt">
       <v-row class="cat-box">
-        <v-col v-for="(box, cat) in boxes" :key="cat">
+        <v-col v-for="box in boxes" :key="box.id">
           <v-card
-            @click="toggleHover(cat)"
+            @click="toggleHover(box.id)"
             class="small-box"
-            :class="{ hover: hoverBoxes === cat }"
+            :class="{ hover: hoverBoxes === box.id }"
           >
-            <v-card-text> Box {{ cat + 1 }} </v-card-text>
+            <v-card-text> Box {{ box.id }} </v-card-text>
           </v-card>
         </v-col>
       </v-row>
     </div>
   </v-app>
 </template>
- <script>
+
+<script>
 export default {
   data() {
     return {
-      boxes: [1, 2, 3, 4, 5, 6],
+      boxes: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }, { id: 6 }],
       hoverBoxes: null,
     };
   },
 
   methods: {
-    toggleHover(cat) {
-      this.hoverBoxes = this.hoverBoxes === cat ? null : cat;
+    toggleHover(boxId) {
+      this.hoverBoxes = this.hoverBoxes === boxId ? null : boxId;
     },
   },
 };
 </script>
- 
- <style>
+
+<style>
 .cat-box {
   width: 752px;
-  height: 231px;
-  flex-shrink: 0;
+  height: 141px;
   border-radius: 35px;
   background: #1e1e1e;
   margin-right: 20px;
+  margin-top: 80px;
 }
 
 .small-box {
+  margin-top: 20px;
   width: 80px; /* Adjust the width as needed */
   height: 60px; /* Adjust the height as needed */
   background-color: #cccccc; /* Gray background color */
